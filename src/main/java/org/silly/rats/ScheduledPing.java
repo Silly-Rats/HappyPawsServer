3f7cc6def919a8400031c8d015d81200de9b626a
Lesnoi Dmytro <dmytrolesnoi@gmail.com>
2024-07-18 19:15:45 +0300

    Removed scheduled pings

--- src/main/java/org/silly/rats/ScheduledPing.java
+++ src/main/java/org/silly/rats/ScheduledPing.java
@@ -2,16 +2,14 @@ package org.silly.rats;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.scheduling.annotation.Scheduled;
-import org.springframework.web.client.RestTemplate;
 
 @Configuration
 @EnableScheduling
 public class ScheduledPing {
-	@Scheduled(fixedDelay = 60000)
-	public void ping() {
-		RestTemplate restTemplate = new RestTemplate();
-		restTemplate.getForEntity("https://happypawsserver.onrender.com/api/ping", String.class);
-		restTemplate.getForObject("https://happypaws-87hv.onrender.com/ping", String.class);
-	}
+//	@Scheduled(fixedDelay = 60000)
+//	public void ping() {
+//		RestTemplate restTemplate = new RestTemplate();
+//		restTemplate.getForEntity("https://happypawsserver.onrender.com/api/ping", String.class);
+//		restTemplate.getForObject("https://happypaws-87hv.onrender.com/ping", String.class);
+//	}
 }
